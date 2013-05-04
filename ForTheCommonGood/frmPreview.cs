bd023ddf9e59bd68f39c2ee898fe882d17d18d24
This, that and the other <at.light@live.com.au>
2013-05-04 11:50:37 +1000

    hide .mw-editsection in preview

--- ForTheCommonGood/frmPreview.cs
+++ ForTheCommonGood/frmPreview.cs
@@ -14,7 +14,7 @@ namespace ForTheCommonGood
             "href=\"http://bits.wikimedia.org/commons.wikimedia.org/load.php?debug=false&amp;lang=en&amp;modules=ext.filepage%7Cmediawiki.legacy.shared%7Cskins.vector&amp;only=styles&amp;skin=vector&amp;*\" />" +
             "<link rel=\"stylesheet\" " +
             "href=\"http://bits.wikimedia.org/commons.wikimedia.org/load.php?debug=false&amp;lang=en&amp;modules=site&amp;only=styles&amp;skin=vector&amp;*\" />" +
-            "<base href=\"http://commons.wikimedia.org/wiki/x\" target=\"_blank\" /><style>body { font-size: 0.8em; }</style>";
+            "<base href=\"http://commons.wikimedia.org/wiki/x\" target=\"_blank\" /><style>body { font-size: 0.8em; } .mw-editsection { display: none; }</style>";
         
         public frmPreview()
         {
