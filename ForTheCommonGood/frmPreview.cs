46091efac82ef7513ea20d40b30d330cdbcb71ed
This, that and the other <at.light@live.com.au>
2014-03-11 20:44:08 +1100

    fix styling in preview window
    Use standards mode, not the default IE7 quirk-y mode. Also update CSS
    URLs

--- ForTheCommonGood/frmPreview.cs
+++ ForTheCommonGood/frmPreview.cs
@@ -10,11 +10,12 @@ namespace ForTheCommonGood
 {
     public partial class frmPreview: Form
     {
-        public const string css = "<!doctype html><link rel=\"stylesheet\" " +
-            "href=\"http://bits.wikimedia.org/commons.wikimedia.org/load.php?debug=false&amp;lang=en&amp;modules=ext.filepage%7Cmediawiki.legacy.shared%7Cskins.vector&amp;only=styles&amp;skin=vector&amp;*\" />" +
+        public const string css = "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" /><link rel=\"stylesheet\" " +
+            "href=\"http://bits.wikimedia.org/commons.wikimedia.org/load.php?debug=false&amp;lang=en&amp;modules=ext.filepage%7Cmediawiki.legacy.shared%7Cskins.common.interface%7Cskins.vector.styles&amp;only=styles&amp;skin=vector&amp;*\" />" +
             "<link rel=\"stylesheet\" " +
             "href=\"http://bits.wikimedia.org/commons.wikimedia.org/load.php?debug=false&amp;lang=en&amp;modules=site&amp;only=styles&amp;skin=vector&amp;*\" />" +
-            "<base href=\"http://commons.wikimedia.org/wiki/x\" target=\"_blank\" /><style>body { font-size: 0.8em; } .mw-editsection { display: none; }</style>";
+            "<base href=\"http://commons.wikimedia.org/wiki/x\" target=\"_blank\" />" + 
+            "<style>body { font-size: 0.8em; background-color: white; color: black; padding: 0.8em; } .mw-editsection { display: none; }</style>";
         
         public frmPreview()
         {
