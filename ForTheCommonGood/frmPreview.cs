9b669ace267bb9e6fec18a2fd354a1cc63cd77b5
This, that and the other <at.light@live.com.au>
2014-08-11 21:02:52 +1000

    update preview styling URLs

--- ForTheCommonGood/frmPreview.cs
+++ ForTheCommonGood/frmPreview.cs
@@ -22,13 +22,17 @@ namespace ForTheCommonGood
 
         public void SetContent(string html)
         {
-            webBrowser1.DocumentText = "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" /><link rel=\"stylesheet\" " +
-                "href=\"http://bits.wikimedia.org/commons.wikimedia.org/load.php?debug=false&amp;lang=en&amp;modules=ext.filepage%7Cmediawiki.legacy.shared%7Cskins.common.interface%7Cskins.vector.styles&amp;only=styles&amp;skin=vector&amp;*\" />" +
+            string text = "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" /><link rel=\"stylesheet\" " +
+                "href=\"http://bits.wikimedia.org/commons.wikimedia.org/load.php?debug=false&amp;lang=en&amp;modules=filepage%7Cmediawiki.legacy.shared%7Cmediawiki.skinning.interface%7Cskins.vector.styles&amp;only=styles&amp;skin=vector&amp;*\" />" +
                 "<link rel=\"stylesheet\" " +
                 "href=\"http://bits.wikimedia.org/commons.wikimedia.org/load.php?debug=false&amp;lang=en&amp;modules=site&amp;only=styles&amp;skin=vector&amp;*\" />" +
                 "<base href=\"http://" + MorebitsDotNet.GetDomain(Wiki.Commons) + "/wiki/x\" target=\"_blank\" />" +
-                "<style>body { font-size: 0.8em; background-color: white; color: black; padding: 0.8em; } .editsection, .mw-editsection { display: none; }</style>" + 
+                "<style>body { font-size: 0.8em; background-color: white; color: black; padding: 0.8em; } .editsection, .mw-editsection { display: none; }</style>" +
                 html;
+            if (webBrowser1.Document == null)
+                webBrowser1.DocumentText = text;
+            else
+                webBrowser1.Document.Write(text);
         }
     }
 }
